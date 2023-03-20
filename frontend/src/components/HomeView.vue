6cfcced6f08829e9b585bcb6fa56d81d2f230236
Stybs <stybse@gmail.com>
2023-03-20 22:06:50 +0100

    Now enters executeMode and sets searchId after creating graph

--- frontend/src/components/HomeView.vue
+++ frontend/src/components/HomeView.vue
@@ -111,7 +111,8 @@ export default {
       const payload = {"Actor": "Foo", "Activities": this.activities, "Relations": this.relations};
       await axios.post(`DCR/create`, payload).then(res => {
         if (res.status == 200) {
-          console.log(res.data)
+          this.searchId = res.data['id'];
+          this.executeMode = true;
         }
       }).catch(err => {
           console.log(err);
