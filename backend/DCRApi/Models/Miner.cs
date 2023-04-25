0dfc587c4c461bef7c563adfd1625e616089a8d7
Jasper Neo <jasper@Jaspers-MacBook-Pro.local>
2023-04-25 19:36:08 +0200

    Evaluation Working

--- backend/DCRApi/Models/Miner.cs
+++ backend/DCRApi/Models/Miner.cs
@@ -1,9 +1,15 @@
 using System.Collections.Concurrent;
+using System.Diagnostics;
 using Microsoft.Extensions.Options;
 
 namespace DCR;
 
-public class Miner : AbstractNode
+public interface IMiner
+{
+    public void Mine();
+    public void CancelMining();
+}
+public class Miner :  AbstractNode, IMiner
 {
     private readonly ILogger<Miner> _logger;
     private readonly ConcurrentQueue<Transaction> _queue = new ConcurrentQueue<Transaction>();
@@ -15,13 +21,11 @@ public class Miner : AbstractNode
 
     public override void HandleTransaction(Transaction tx)
     {
-        if (IsValidTransaction(tx)) {
-            lock (_queue)
+        lock (_queue)
+        {
+            if (!_queue.Any(t => t.Id == tx.Id))
             {
-                if (!_queue.Any(t => t.Id == tx.Id))
-                {
-                    _queue.Enqueue(tx);
-                }
+                _queue.Enqueue(tx);
             }
         }
     }
@@ -32,8 +36,13 @@ public class Miner : AbstractNode
         List<Transaction> txs = new List<Transaction>();
         Transaction? transaction;
         int i = 0;
-        while (i < Settings.SizeOfBlocks) // Blocks contain 10 transactions
+        Console.WriteLine("d");
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        Console.WriteLine("c");
+        while (stopwatch.ElapsedMilliseconds < 4000 && i < Settings.SizeOfBlocks)
         {
+            Console.WriteLine($"Elapsed : {stopwatch.ElapsedMilliseconds}");
             _queue.TryDequeue(out transaction);
             if (transaction is null)
             {
@@ -43,16 +52,22 @@ public class Miner : AbstractNode
             {
                 if (!Blockchain.Chain.Any(b => b.Transactions.Any(t => t.Id == transaction.Id)))
                 {
-                    txs.Add(transaction);
-                    i++;
+                    // Check transaction validity if doing evaluation with lazy evaluation
+                    if (IsValidTransaction(transaction))
+                    {
+                        txs.Add(transaction);
+                        i++;
+                    }
                 }
             }
         }
+        Console.WriteLine($"Spent {stopwatch.Elapsed} ms mining");
+        stopwatch.Stop();
         Thread.Sleep(Settings.TimeToSleep); // For testing, a block is added every 15 seconds
         var newBlock = Blockchain.MineTransactions(txs, miningCT);
         if (!miningCT.IsCancellationRequested)
         {
-            ShareBlock(newBlock);
+            if (!Settings.IsEval) ShareBlock(newBlock);
             Save();
         }
         if (miningCT.IsCancellationRequested)
@@ -60,6 +75,10 @@ public class Miner : AbstractNode
             Console.WriteLine("Cancellation was requested");
             miningCTSource = new CancellationTokenSource();
         }
+        if(Settings.IsEval)
+        {
+            return;
+        } 
     }
 
     public void CancelMining()
---
+++ backend/Evaluation/Program.cs
@@ -0,0 +1,12 @@
+﻿using DCR;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Evaluation eval = new Evaluation();
+            eval.Eval();
+        }
+    }
+}
