4bf4861ea0b810e62eb2bfc971ddeda572b27d4b
Jasper Neo <jasper@Jaspers-MacBook-Pro.local>
2023-05-19 20:20:12 +0200

    Added Validation test and fixed Eval

--- backend/DCRApi/Tests/MinerTests.cs
+++ backend/DCRApi/Tests/MinerTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Diagnostics;
 using DCR;
+using Newtonsoft.Json;
 
 namespace Tests;
 
@@ -23,7 +24,7 @@ public class MinerTests
             SizeOfBlocks = int.MaxValue,
             NumberNeighbours = 1,
             Difficulty = 0,
-            NumEvalTransactions = 50000
+            NumEvalTransactions = 10000
         };
         _miner = new Miner(logger, networkClient, _settings);
     }
@@ -31,10 +32,8 @@ public class MinerTests
     [Test]
     public void Evaluate_DequeueTransactions()
     {
-        var graphJson = """{"Actor":"Foo","Activities":[{"title":"Select papers","pending":true,"included":true,"executed":false,"enabled":true},{"title":"Write introduction","pending":true,"included":true,"executed":false,"enabled":false},{"title":"Write abstract","pending":true,"included":true,"executed":false,"enabled":false},{"title":"Write conclusion","pending":true,"included":true,"executed":false,"enabled":false}],"Relations":[{"source":"Select papers","type":2,"target":"Select papers"},{"source":"Select papers","type":0,"target":"Write introduction"},{"source":"Select papers","type":0,"target":"Write abstract"},{"source":"Select papers","type":0,"target":"Write conclusion"},{"source":"Write introduction","type":1,"target":"Write abstract"},{"source":"Write conclusion","type":1,"target":"Write abstract"}]}""";
-
         // Enqueue Create transactions
-        EnqueueTransactions(graphJson, _settings.NumEvalTransactions);
+        EnqueueTransactions(_settings.NumEvalTransactions);
 
         // Evaluated transactions dequeue/validation
         var stopwatch = new Stopwatch();
@@ -53,18 +52,15 @@ public class MinerTests
     [Test]
     public void Evaluate_DequeueTransactions_FilledBlockchain()
     {
-        var graphJson = """{"Actor":"Foo","Activities":[{"title":"Select papers","pending":true,"included":true,"executed":false,"enabled":true},{"title":"Write introduction","pending":true,"included":true,"executed":false,"enabled":false},{"title":"Write abstract","pending":true,"included":true,"executed":false,"enabled":false},{"title":"Write conclusion","pending":true,"included":true,"executed":false,"enabled":false}],"Relations":[{"source":"Select papers","type":2,"target":"Select papers"},{"source":"Select papers","type":0,"target":"Write introduction"},{"source":"Select papers","type":0,"target":"Write abstract"},{"source":"Select papers","type":0,"target":"Write conclusion"},{"source":"Write introduction","type":1,"target":"Write abstract"},{"source":"Write conclusion","type":1,"target":"Write abstract"}]}""";
+        var graphJsonWithId = """{"Id":"evalgraph","Activities":[{"Title":"Select papers","Pending":true,"Included":true,"Executed":false,"Enabled":true},{"Title":"Write introduction","Pending":true,"Included":true,"Executed":false,"Enabled":false},{"Title":"Write abstract","Pending":true,"Included":true,"Executed":false,"Enabled":false},{"Title":"Write conclusion","Pending":true,"Included":true,"Executed":false,"Enabled":true}],"Relations":[{"Type":2,"Source":"Select papers","Target":"Select papers"},{"Type":0,"Source":"Select papers","Target":"Write introduction"},{"Type":0,"Source":"Select papers","Target":"Write abstract"},{"Type":2,"Source":"Select papers","Target":"Write conclusion"},{"Type":1,"Source":"Write introduction","Target":"Write abstract"},{"Type":1,"Source":"Write conclusion","Target":"Write abstract"}],"Accepting":false}"""; 
 
         // Enqueue Create transactions
-        int sizeOfBlockchain = 50000;
+        int sizeOfBlockchain = 1;
         // Evaluated transactions dequeue/validation
         var stopwatch = new Stopwatch();
         var cancellationTokenSource = new CancellationTokenSource();
         var cancellationToken = cancellationTokenSource.Token;
-        EnqueueTransactions(graphJson, sizeOfBlockchain);
-        var actualTxsPrev = _miner.DequeueTransactions(cancellationToken);
-
-        EnqueueTransactions(graphJson, sizeOfBlockchain);
+        EnqueueTransactionsWithExecutions(graphJsonWithId, _settings.NumEvalTransactions, sizeOfBlockchain);
         stopwatch.Start();
         cancellationTokenSource.CancelAfter(4000);
         var actualTxs = _miner.DequeueTransactions(cancellationToken);
@@ -75,11 +71,83 @@ public class MinerTests
         Assert.AreEqual(_settings.NumEvalTransactions, actualTxs.Count());
     }
 
-    private void EnqueueTransactions(string graphJson, int numTransactions) {
+    [Test]
+    public void TestValidation()
+    {
+        var graphJsonWithId = """{"Id":"evalgraph","Activities":[{"Title":"Select papers","Pending":true,"Included":true,"Executed":false,"Enabled":true},{"Title":"Write introduction","Pending":true,"Included":true,"Executed":false,"Enabled":false},{"Title":"Write abstract","Pending":true,"Included":true,"Executed":false,"Enabled":false},{"Title":"Write conclusion","Pending":true,"Included":true,"Executed":false,"Enabled":true}],"Relations":[{"Type":2,"Source":"Select papers","Target":"Select papers"},{"Type":0,"Source":"Select papers","Target":"Write introduction"},{"Type":0,"Source":"Select papers","Target":"Write abstract"},{"Type":2,"Source":"Select papers","Target":"Write conclusion"},{"Type":1,"Source":"Write introduction","Target":"Write abstract"},{"Type":1,"Source":"Write conclusion","Target":"Write abstract"}],"Accepting":false}"""; 
+
+        // Enqueue Create transactions
+        int sizeOfBlockchain = 1;
+        // Evaluated transactions dequeue/validation
+        var stopwatch = new Stopwatch();
+        var cancellationTokenSource = new CancellationTokenSource();
+        var cancellationToken = cancellationTokenSource.Token;
+        CreateTransactionsWithInvalidId(graphJsonWithId, _settings.NumEvalTransactions, sizeOfBlockchain);
+        var noTxs = _miner.DequeueTransactions(cancellationToken);
+        Assert.AreEqual(0, noTxs.Count());
+        EnqueueTransactionsWithExecutions(graphJsonWithId, 1000, 1);
+        var txs = _miner.DequeueTransactions(cancellationToken);
+        Assert.AreEqual(1000, txs.Count());
+    }
+
+    private Models.Graph CreateGraph()
+    {
+        List<Models.Activity> activities = new List<Models.Activity>(){
+            new Models.Activity("Select papers", true, true, false), 
+            new Models.Activity("Write introduction", true, true, false), 
+            new Models.Activity("Write abstract", true, true, false), 
+            new Models.Activity("Write conclusion", true, true, false)};
+        List<Models.Relation> relations = new List<Models.Relation>(){
+            new Models.Relation(Models.RelationType.INCLUSION, "Select papers", "Select papers"),
+            new Models.Relation(Models.RelationType.CONDITION, "Select papers", "Write introduction"),
+            new Models.Relation(Models.RelationType.CONDITION, "Select papers", "Write abstract"),
+            new Models.Relation(Models.RelationType.INCLUSION, "Select papers", "Write conclusion"),
+            new Models.Relation(Models.RelationType.RESPONSE, "Write introduction", "Write abstract"),
+            new Models.Relation(Models.RelationType.RESPONSE, "Write conclusion", "Write abstract"),
+        };
+        return new Models.Graph(activities, relations);
+    }
+    private void EnqueueTransactions(int numTransactions) {
         for (int i = 0; i < numTransactions; i++) {
-            var graph = _graphSerializer.Deserialize(graphJson);
+            Models.Graph graph = CreateGraph();
             var tx = new Transaction("eval", DCR.Action.Create, "", graph);
             _miner.HandleTransaction(tx);
         }
+    }
+    private void EnqueueTransactionsWithExecutions(string graphJsonWithId, int numTransactions, int blockchainSize) {
+        var graph = _graphSerializer.Deserialize(graphJsonWithId);
+        var createTx = new Transaction("eval", DCR.Action.Create, "", graph);
+        _miner.HandleTransaction(createTx);
+        HandleOtherGraphs(blockchainSize);
+        for (int i = 0; i < numTransactions; i++) {
+            var graphToUpdate = _graphSerializer.Deserialize(graphJsonWithId);
+            graphToUpdate.Execute("Select papers");
+            var tx = new Transaction("1", DCR.Action.Update, "Select papers", graphToUpdate);
+            _miner.HandleTransaction(tx);
+        }
+    }
+    private void CreateTransactionsWithInvalidId(string graphJsonWithId, int numTransactions, int blockchainSize) {
+        var graph = _graphSerializer.Deserialize(graphJsonWithId);
+        var createTx = new Transaction("eval", DCR.Action.Create, "", graph);
+        _miner.HandleTransaction(createTx);
+        HandleOtherGraphs(blockchainSize);
+        for (int i = 0; i < numTransactions; i++) {
+            var graphJsonWithWrongId = """{"Id":"Foo","Activities":[{"Title":"Select papers","Pending":true,"Included":true,"Executed":false,"Enabled":true},{"Title":"Write introduction","Pending":true,"Included":true,"Executed":false,"Enabled":false},{"Title":"Write abstract","Pending":true,"Included":true,"Executed":false,"Enabled":false},{"Title":"Write conclusion","Pending":true,"Included":true,"Executed":false,"Enabled":true}],"Relations":[{"Type":2,"Source":"Select papers","Target":"Select papers"},{"Type":0,"Source":"Select papers","Target":"Write introduction"},{"Type":0,"Source":"Select papers","Target":"Write abstract"},{"Type":2,"Source":"Select papers","Target":"Write conclusion"},{"Type":1,"Source":"Write introduction","Target":"Write abstract"},{"Type":1,"Source":"Write conclusion","Target":"Write abstract"}],"Accepting":false}"""; 
+            var graphToUpdate = _graphSerializer.Deserialize(graphJsonWithWrongId);
+            graphToUpdate.Execute("Select papers");
+            var tx = new Transaction("1", DCR.Action.Update, "Select papers", graphToUpdate);
+            _miner.HandleTransaction(tx);
+        }
+    }
+
+    private void HandleOtherGraphs(int blockchainSize)
+    {
+        for (int i = 1; i < blockchainSize; i++)
+        {
+            var graph = CreateGraph();
+            var createTx = new Transaction("eval", DCR.Action.Create, "", graph);
+            _miner.HandleTransaction(createTx);
+        }
+        _miner.Mine();
     }
 }
