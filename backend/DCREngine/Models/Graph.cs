1c5cbc894520cdd0be271547a14804924bdcd03a
Stybs <stybse@gmail.com>
2023-05-19 17:32:36 +0200

    Adjusted access modifier for Graph methods

--- backend/DCREngine/Models/Graph.cs
+++ backend/DCREngine/Models/Graph.cs
@@ -7,11 +7,11 @@ namespace Models
         public List<Activity> Activities { get; set; }
         public List<Relation> Relations { get; set; }
 
-        public Graph():this(Guid.NewGuid().ToString(), new List<Activity>(), new List<Relation>())
+        public Graph() : this(Guid.NewGuid().ToString(), new List<Activity>(), new List<Relation>())
         {
         }
 
-        public Graph(List<Activity> activities, List<Relation> relations):this(Guid.NewGuid().ToString(), activities, relations)
+        public Graph(List<Activity> activities, List<Relation> relations) : this(Guid.NewGuid().ToString(), activities, relations)
         {
         }
 
@@ -24,19 +24,31 @@ namespace Models
             UpdateActivitiesEnabled();
         }
 
-        public bool Accepting {
-            get {
+        public bool Accepting
+        {
+            get
+            {
                 return Activities.All(e => !(e.Included && e.Pending));
             }
         }
 
+        private Activity GetActivity(string title)
+        {
+            return Activities.Single(a => a.Title == title);
+        }
+
         // Update the enabled status of all activities in graph
-        public void UpdateActivitiesEnabled() {
-            foreach (Activity activity in Activities) {
+        private void UpdateActivitiesEnabled()
+        {
+            foreach (Activity activity in Activities)
+            {
                 // If activity is not included, disable
-                if (!activity.Included) {
+                if (!activity.Included)
+                {
                     activity.Enabled = false;
-                } else {
+                }
+                else
+                {
                     // If not all included conditions are met, disable
                     var conditions = Relations.Where(r => (r.Type == RelationType.CONDITION) && (GetActivity(r.Source).Included) && (r.Target == activity.Title));
                     activity.Enabled = conditions.All(r => GetActivity(r.Source).Executed);
@@ -53,13 +65,19 @@ namespace Models
 
             // Get relations where input activity is source
             var relations = Relations.Where(e => e.Source == source.Title);
-            foreach (Relation rel in relations) {
+            foreach (Relation rel in relations)
+            {
                 var target = GetActivity(rel.Target);
-                if (rel.Type == RelationType.EXCLUSION) {
+                if (rel.Type == RelationType.EXCLUSION)
+                {
                     target.Included = false;
-                } else if (rel.Type == RelationType.INCLUSION) {
+                }
+                else if (rel.Type == RelationType.INCLUSION)
+                {
                     target.Included = true;
-                } else if (rel.Type == RelationType.RESPONSE) {
+                }
+                else if (rel.Type == RelationType.RESPONSE)
+                {
                     target.Pending = true;
                 }
             }
@@ -67,15 +85,10 @@ namespace Models
             UpdateActivitiesEnabled();
         }
 
-        public Activity GetActivity(string title)
-        {
-            return Activities.Single(a => a.Title == title);
-        }
-
         public bool EqualsGraph(Graph otherGraph)
         {
             if (otherGraph is null) return false;
-            return  (Id == otherGraph.Id) &&
+            return (Id == otherGraph.Id) &&
                     (Relations.SequenceEqual(otherGraph.Relations)) &&
                     (Activities.SequenceEqual(otherGraph.Activities)) &&
                     (Accepting == otherGraph.Accepting);
